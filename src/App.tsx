4930ce8fa7284cbc38380d3d902f35ac6220f6e9
harshilpradhan <68425870+harshilpradhan@users.noreply.github.com>
2023-04-11 14:48:09 +0530

    Revert "Add a navbar to home page"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import { Header } from './components';
-
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+  return <div className="App">Test</div>;
 }
 
 export default App;
